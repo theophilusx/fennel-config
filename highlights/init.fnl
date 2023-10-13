7ac9180affcf3d3303035af4b0ce56e33aa28db5
Tim Cross <theophilusx@gmail.com>
2023-10-13 23:53:03 +1100

    Add improved paren mathcing colors

--- highlights/init.fnl
+++ highlights/init.fnl
@@ -1,4 +1,5 @@
 {;; this table overrides highlights in all themes
  ;; :Normal {:bg "#000000"}
+ :MatchParen {:bg "#FF0000" :fg "#FFFF00"}
  }
 
--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,2 @@
-return { -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
-}
+-- [nfnl] Compiled from highlights/init.fnl by https://github.com/Olical/nfnl, do not edit.
+return {MatchParen = {bg = "#FF0000", fg = "#FFFF00"}}
